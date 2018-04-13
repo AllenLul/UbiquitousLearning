451c99cc92241fb42635c553fe9fdbab1d4e8608
llf <1172593703@qq.com>
2018-04-13 15:52:18 +0800

    性别枚举

---
+++ src/main/java/just/learn/common/enums/GenderEnum.java
@@ -0,0 +1,32 @@
+package just.learn.common.enums;
+
+/**
+ * Created by llf on 2018/4/13.
+ */
+public enum GenderEnum {
+    MALE("male","男"),FEMALE("female","女");
+    private String  value;
+    private String description;
+    GenderEnum(String value,String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getDescription(String value) {
+        if (value==null||value.isEmpty()){
+            return  null;
+        }
+        for (GenderEnum ge:GenderEnum.values()) {
+            if (value.equals(ge.value)){
+                return ge.description;
+            }
+        }
+        return null;
+    }
+}
