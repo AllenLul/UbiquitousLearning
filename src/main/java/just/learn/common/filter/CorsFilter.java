ef61ddfa859e6bcf5536e59842d461874c6e1802
llf <1172593703@qq.com>
2018-05-09 11:43:00 +0800

    JWT

--- src/main/java/just/learn/common/filter/CorsFilter.java
+++ src/main/java/just/learn/common/filter/CorsFilter.java
@@ -1,5 +1,7 @@
 package just.learn.common.filter;
 
+import org.apache.commons.digester.annotations.rules.SetProperty;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -11,10 +13,12 @@ import java.io.IOException;
  * Created by admin on 2018/5/8.
  */
 @Component
+@Order(0)
 public class CorsFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+
 
     }
 
@@ -23,7 +27,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest request= (HttpServletRequest) servletRequest;
         HttpServletResponse response= (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin",request.getHeader("origin"));
-        response.setHeader("Access-Control-Allow-Origin","*");  //允许跨域访问的域
+        response.setHeader("Access-Control-Allow-Origin","localhost:8080");  //允许跨域访问的域
         response.setHeader("Access-Control-Allow-Methods","POST,GET,OPTIONS,DELETE,PUT");  //允许使用的请求方法
         response.setHeader("Access-Control-Expose-Headers","*");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Cache-Control,Pragma,Content-Type,Authorization");  //允许使用的请求方法
